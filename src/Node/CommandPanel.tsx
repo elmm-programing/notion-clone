783827db3c086fae5860628d99f9eb6e666d26a7
ELMM <elmm.programing@gmail.com>
2024-04-07 19:41:47 -0400

    Created CommandPanel component

---
+++ src/Node/CommandPanel.tsx
@@ -0,0 +1,78 @@
+import {  useEffect, useState } from "react";
+import { nodeType } from "../utils/types";
+import { useOverflowScreenBottom } from "./useOverflowScreenBottom";
+import styles from "./CommandPanel.module.css"
+import cx from "classnames"
+
+type CommandPanelProps = {
+  nodeText: string;
+  selectItem: (nodeType: nodeType) => void
+}
+
+type SupportedNodeType = {
+  value: nodeType;
+  name: string;
+}
+const supportedNodeTypes: SupportedNodeType[] = [
+  { value: "text", name: "Text" },
+  { value: "list", name: "List" },
+  { value: "heading1", name: "Heading 1" },
+  { value: "heading2", name: "Heading 2" },
+  { value: "heading3", name: "Heading 3" },
+]
+
+export const CommandPanel = ({ nodeText, selectItem }: CommandPanelProps) => {
+  const [selectedItemIndex, setSelectedItemIndex] = useState(0)
+  const { overflows, ref } = useOverflowScreenBottom()
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Enter") {
+        selectItem(supportedNodeTypes[selectedItemIndex].value)
+
+      }
+    }
+    window.addEventListener("keydown",handleKeyDown)
+    return()=>{
+
+    window.removeEventListener("keydown",handleKeyDown)
+    }
+
+  }, [selectedItemIndex, selectItem])
+
+  useEffect(() => {
+    const normalizedValue = nodeText.toLowerCase().replace(/\//,"")
+    setSelectedItemIndex(
+      supportedNodeTypes.findIndex(item=>item.value.match(normalizedValue))
+    )
+
+  },[nodeText])
+
+  return (
+  <div ref={ref}
+      className={
+        cx(styles.panel,{
+          [styles.reverse]:overflows,
+        })
+      }
+    >
+      <div className={styles.title}>Blocks</div>
+      <ul>
+        {supportedNodeTypes.map((type,index)=>{
+          const selected =selectedItemIndex === index;
+          return <li
+            key={type.value}
+            className={ 
+              cx({
+                [styles.selected]:selected
+              })
+
+            }
+            onClick={()=>selectItem(type.value)}
+          >{type.name}</li>
+
+        })}
+      </ul>
+
+    </div>
+  )
+}
